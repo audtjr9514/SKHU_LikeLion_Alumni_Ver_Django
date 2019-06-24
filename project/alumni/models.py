265eb0eeb6e2683d778279a782f8c48b63bc8375
myeongs14 <audtjr9514@gmail.com>
2019-06-25 01:29:06 +0900

    19.06.25 해커톤 포트폴리오 작성 완료

--- project/alumni/models.py
+++ project/alumni/models.py
@@ -10,11 +10,14 @@ class Member(models.Model):
     name = models.CharField(max_length=10)          # 이름
     major = models.CharField(max_length=15)         # 학과
     enter_year = models.IntegerField()              # 입학년도
-    enter_term = models.ForeignKey('Term', on_delete=models.CASCADE, related_name="enter_term") # 활동한 기수
-    tutor = models.ManyToManyField('Term', related_name="tutor", blank=True) # 운영진 기수
-    # 한 회원이 여러번의 운영진을 할 수 있고, 한 기수에 여러명이 있을 수 있다.
+    enter_term = models.ForeignKey('Term', on_delete=models.CASCADE, related_name="enter_members") 
+                                                    # 활동한 기수
+    tutor = models.ManyToManyField('Term', blank=True, related_name="tutors") 
+                                                    # 운영진 기수
+                                                    # 한 회원이 여러번의 운영진을 할 수 있고, 한 기수에 여러명이 있을 수 있다.
     image = models.CharField(max_length=30)         # 사진
-    portfolio = models.ManyToManyField('Port', blank=True)
+    portfolio = models.ManyToManyField('Port', blank=True, related_name="port_members")
+                                                    # 해커톤 프로젝트
     def __str__(self):
         return self.name
 
@@ -22,7 +25,7 @@ class Member(models.Model):
 class Port(models.Model):
     title = models.CharField(max_length=30)         # 제목
     content = models.TextField()                    # 내용
-    Image = models.CharField(max_length=30)  # 사진
+    Image = models.CharField(max_length=30)         # 사진
     def __str__(self):
         return self.title
 
--- project/alumni/views.py
+++ project/alumni/views.py
@@ -3,28 +3,22 @@ from .models import Member, Term, Port
 # Create your views here.
 
 # 첫 화면
-def index(request):    
-    terms = Term.objects.all()
-    return render(request, 'index.html',{'terms':terms})
+def index(request):
+    return render(request, 'index.html')
 
 # 현재까지 멋쟁이 사자처럼의 회원
 def members(request):
-    # members = Member.objects.all()
-    # terms = Term.objects.all()
-    members = list()
+    members = Member.objects.all()
     terms = Term.objects.all()
-    # for termm in terms:
-    #     if Member.objects.filter(enter_term=termm.term).count() > 0 :
-    #         members.append(Member.objects.filter(enter_term=termm.term))
-    members.append(Member.objects.filter(enter_term=4))
-    members.append(Member.objects.filter(enter_term=5))
-    members.append(Member.objects.filter(enter_term=6))
-    return render(request, 'members.html', {'terms':terms , 'memberss':members})
+    return render(request, 'members.html', {'terms':terms, 'members':members})
   
 # 현재까지 멋쟁이 사자처럼의 운영진
 def tutors(request):
-    return render(request, 'tutors.html')
+    members = Member.objects.all()
+    terms = Term.objects.all()
+    return render(request, 'tutors.html', {'terms':terms, 'members':members})
 
 # 현재까지 멋쟁이 사자처럼의 해커톤 출전 내용
 def portfolios(request):
-    return render(request, 'portfolios.html')
+    ports = Port.objects.all()
+    return render(request, 'portfolios.html', {'ports':ports, 'members':members})
